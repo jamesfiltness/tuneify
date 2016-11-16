3366dc62db105dd34c1f7009e5e025af0365c779
James Filtness <james.filtness@uk.tesco.com>
2016-11-16 19:28:53 +0000

    Bootstrap the unit test enviroment using mocha, chai, enzyme

--- app/components/app/spec.js
+++ app/components/app/spec.js
@@ -1,7 +1,13 @@
-describe('test', function() {
-  describe('test', function() {
-    it('test', function() {
-      return true;
-    });
+import React from 'react';
+import { App } from './';
+
+describe('App component', function() {
+  let component;
+  beforeEach(() => {
+    component = shallow(<App />);
+  });
+
+  it('renders the component wrapper', function() {
+    expect(component.find('.app')).to.have.length(1);
   });
 });
--- test/helper.js
+++ test/helper.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { expect } from 'chai';
+// TODO: comment this file
+import chai  from 'chai';
+import chaiEnzyme from 'chai-enzyme';
 import { shallow, mount, render } from 'enzyme';
 
 const jsdom = require('jsdom').jsdom;
@@ -7,6 +8,8 @@ const jsdom = require('jsdom').jsdom;
 const exposedProperties = ['window', 'navigator', 'document'];
 
 global.document = jsdom('');
+
+//grab the window object out of the document
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
@@ -19,9 +22,11 @@ global.navigator = {
   userAgent: 'node.js'
 };
 
-global.expect = expect;
+
+chai.use(chaiEnzyme());
+
+global.expect = chai.expect;
 global.mount = mount;
 global.shallow = shallow;
 global.render = render;
 
-documentRef = document;
