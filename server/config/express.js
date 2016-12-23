0ddb771c1167d960569a68695fb0b37e7358d974
James Filtness <jamesfiltness@gmail.com>
2016-12-23 08:21:43 +0000

    Add cookie parser

--- server/config/express.js
+++ server/config/express.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
-
+import cookieParser from 'cookie-parser';
 import isomorphic from '../routes/isomorphic';
 
 export default function() {
 const app = express();
+app.use(cookieParser());
 
 // TODO: Remove webpackdevmiddleare and use standard webpack approach
 // Use https://www.npmjs.com/package/babel-plugin-transform-require-ignore to ignore css imports (CSS modules)
