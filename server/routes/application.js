3cba5d017111f0b482403f5efdd2ebd85e982526
James Filtness <jamesfiltness@gmail.com>
2016-12-30 16:23:59 +0000

    Rename isomorphic route as app is no longer isomorphic

---
+++ server/routes/application.js
@@ -0,0 +1,71 @@
+import config from 'config';
+import React from 'react';
+import createStore from '../../app/redux/create';
+import { match, RouterContext } from 'react-router';
+
+const store = createStore(
+  // Placeholder data until server app is built
+  {
+    currentSearch: '',
+    autocomplete: {
+      autocompleteArtistData : [],
+      autocompleteAlbumData : [],
+      autocompleteTrackData : [],
+    },
+    currentVideo: '',
+    currentTrackSummaryData: {},
+    videoData: [],
+    albumPage: {
+      albumPageData: null,
+      currentAlbumPageError: null,
+    },
+    artistPage: {
+      artistPageData: null,
+      currentArtistPageError: null,
+    },
+    topArtists : {
+      topArtistData : null,
+      topArtistDataError: null,
+    },
+    playQueue: {
+      playQueueCurrentIndex: 0,
+      playQueueTracks: [],
+      shuffle: false,
+      repeat: false,
+    },
+  }
+);
+
+// pass the necessary config down to the client
+// this makes it easy to manage stuff like mocking api urls
+// configurable via NODE_ENV
+const clientConfig = {
+  endpoints : config.get('endpoints')
+}
+
+export default function(app) {
+  app.get('*', (req, res) => {
+        const serverState = store.getState();
+        const HTML = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <title>Tuneify</title>
+            <link rel=styleSheet href="/styles.css" type="text/css" />
+          </head>
+          <body>
+            <div id="react-view"></div>
+            <script>
+              window.__PRELOADED_STATE__ = ${JSON.stringify(serverState)}
+              window.clientConfig = ${JSON.stringify(clientConfig)}
+            </script>
+            <script src="https://apis.google.com/js/api.js"></script>
+            <script src="http://cdn.auth0.com/js/lock/10.7.3/lock.min.js"></script>
+            <script type="application/javascript" src="/bundle.js"></script>
+          </body>
+        </html>
+        `;
+        res.status(200).send(HTML);
+    });
+}
