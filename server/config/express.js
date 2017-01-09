1ab9df3078de8bc57e610e3d5d1a26cf20b9a196
James Filtness <jamesfiltness@gmail.com>
2017-01-09 20:17:58 +0000

    Rename function to playlist

--- server/config/express.js
+++ server/config/express.js
@@ -8,7 +8,6 @@ export default function() {
 const app = express();
 app.use(cookieParser());
 
-// TODO: Remove webpackdevmiddleare and use standard webpack approach
 // Use https://www.npmjs.com/package/babel-plugin-transform-require-ignore to ignore css imports (CSS modules)
 if (process.env.NODE_ENV !== 'production') {
   const devConfig = require('../../webpack.dev.config.js');
