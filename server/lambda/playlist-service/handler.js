9aaf82df6d0c3a917a592feae50f72d5bc31dd9a
James Filtness <jamesfiltness@gmail.com>
2017-01-13 22:29:20 +0000

    Add placeholder DynamoDb query and infra

--- server/lambda/playlist-service/handler.js
+++ server/lambda/playlist-service/handler.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+var jwt = require('jsonwebtoken');
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+// Replace this function with something based on the official AWS customAuthorizer blueprint
 function generatePolicyDocument(principalId, effect, resource) {
 	var authResponse = {};
 	authResponse.principalId = principalId;
@@ -18,21 +24,38 @@ function generatePolicyDocument(principalId, effect, resource) {
 }
 
 module.exports.Playlist = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  var params = {
+    TableName: 'playlists',
+    Item: {
+      id: 'ea92a194-2d60-35c7-9d56-0e1dba20cd45',
+      tracks: [
+        {
+          artist: 'Radiohead', track: 'Airbag'
+        }
+      ]
+    }
+  }
 
-  callback(null, response);
+  docClient.put(params, function(err, data) {
+    if (err) { 
+      callback(err);
+    }
+    else { 
+      callback(null, {});
+    }
+  });
 };
 
+
 module.exports.Authorise = (event, context, callback) => {
-  if (event.authorizationToken === 'pass') {
-    context.succeed(generatePolicyDocument('jamesasid', 'Allow', event.methodArn));
-  } else {
-    context.fail();
-  }
+  var token = event.authorizationToken;
+  var secret = process.env.AUTH0_SECRET;
+  
+  jwt.verify(token, secret, function(err, decoded) {
+    if (err) {
+      callback("Unauthorized");
+    } else if (decoded) {
+      callback(null, generatePolicyDocument('jamesasid', 'Allow', event.methodArn));
+    }
+  });
 };
