d24e72dc35693068df24ed0a382e25bf3472cf04
James Filtness <jamesfiltness@gmail.com>
2017-01-14 20:24:17 +0000

    Split Authentication lambda out in to separate service

--- server/lambda/playlist-service/handler.js
+++ server/lambda/playlist-service/handler.js
@@ -1,30 +1,11 @@
 'use strict';
 
-var AWS = require('aws-sdk');
-var jwt = require('jsonwebtoken');
+const AWS = require('aws-sdk');
 
-var docClient = new AWS.DynamoDB.DocumentClient();
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-// Replace this function with something based on the official AWS customAuthorizer blueprint
-function generatePolicyDocument(principalId, effect, resource) {
-	var authResponse = {};
-	authResponse.principalId = principalId;
-	if (effect && resource) {
-		var policyDocument = {};
-		policyDocument.Version = '2012-10-17'; // default version
-		policyDocument.Statement = [];
-		var statementOne = {};
-		statementOne.Action = 'execute-api:Invoke'; // default action
-		statementOne.Effect = effect;
-		statementOne.Resource = resource;
-		policyDocument.Statement[0] = statementOne;
-		authResponse.policyDocument = policyDocument;
-	}
-	return authResponse;
-}
-
-module.exports.Playlist = (event, context, callback) => {
-  var params = {
+module.exports.playlist = (event, context, callback) => {
+  const params = {
     TableName: 'playlists',
     Item: {
       id: 'ea92a194-2d60-35c7-9d56-0e1dba20cd45',
@@ -41,21 +22,16 @@ module.exports.Playlist = (event, context, callback) => {
       callback(err);
     }
     else { 
-      callback(null, {});
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          data: data,
+          input: event,
+        }),
+      };
+      
+      callback(null, response);
     }
   });
 };
 
-
-module.exports.Authorise = (event, context, callback) => {
-  var token = event.authorizationToken;
-  var secret = process.env.AUTH0_SECRET;
-  
-  jwt.verify(token, secret, function(err, decoded) {
-    if (err) {
-      callback("Unauthorized");
-    } else if (decoded) {
-      callback(null, generatePolicyDocument('jamesasid', 'Allow', event.methodArn));
-    }
-  });
-};
