e24066c648efb3e40fc84d9126246829545f6cbb
James Filtness <jamesfiltness@gmail.com>
2017-01-15 17:06:54 +0000

    Query and Put operations functioning

--- server/lambda/playlist-service/handler.js
+++ server/lambda/playlist-service/handler.js
@@ -5,34 +5,64 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.playlist = (event, context, callback) => {
-  const params = {
-    TableName: 'users',
-    Item: {
-      id: 'ea92a194-2d60-35c7-9d56-0e1dba20cd45',
-      playlists: [
-        {
-          id: 492635,
-          name: 'My Sunday Playlist' 
-        }
-      ]
-    }
-  }
+//  const params = {
+//    TableName: 'playlists',
+//    Item: {
+//      id: 'aa22a194-2d60-35c7-9d56-0e1dba20cd45',
+//      userid: 'some-user-id',
+//      tracks: [
+//        {
+//          id: 492635,
+//          name: 'My Sunday Playlist' 
+//        },
+//        {
+//          id: 555987,
+//          name: 'Another brilliant playlist'
+//        }
+//      ]
+//    }
+//  }
+console.log('event: ', event);
+console.log('context:', context);
 
-  docClient.put(params, function(err, data) {
-    if (err) { 
-      callback(err);
-    }
-    else { 
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          data: data,
-          input: event,
-        }),
-      };
-      
-      callback(null, response);
-    }
-  });
+var params = {
+  TableName: 'playlists',
+  IndexName: 'PlaylistUsers',
+  KeyConditionExpression: "userid = :a",
+  ExpressionAttributeValues: {
+    ":a": "some-user-id"
+  }
+}
+
+docClient.query(params, function(err, data) {
+  if (err) { 
+    console.log(err);
+  } else { 
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: data
+      }),
+    };
+    callback(null, response);
+  }
+});
+//  docClient.put(params, function(err, data) {
+//    if (err) { 
+//      callback(err);
+//    }
+//    else { 
+//      const response = {
+//        statusCode: 200,
+//        body: JSON.stringify({
+//          data: data,
+//          input: event,
+//        }),
+//      };
+//      
+//      callback(null, response);
+//    }
+//  });
+
 };
 
