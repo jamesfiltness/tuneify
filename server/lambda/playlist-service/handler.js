593498718b80590af11c7bdb2b12c12bc0e6da60
James Filtness <jamesfiltness@gmail.com>
2017-01-16 08:58:03 +0000

    Make GET and POST endpoints to function correctly

--- server/lambda/playlist-service/handler.js
+++ server/lambda/playlist-service/handler.js
@@ -1,68 +1,67 @@
-'use strict';
-
 const AWS = require('aws-sdk');
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-module.exports.playlist = (event, context, callback) => {
-//  const params = {
-//    TableName: 'playlists',
-//    Item: {
-//      id: 'aa22a194-2d60-35c7-9d56-0e1dba20cd45',
-//      userid: 'some-user-id',
-//      tracks: [
-//        {
-//          id: 492635,
-//          name: 'My Sunday Playlist' 
-//        },
-//        {
-//          id: 555987,
-//          name: 'Another brilliant playlist'
-//        }
-//      ]
-//    }
-//  }
-console.log('event: ', event);
-console.log('context:', context);
-
-var params = {
-  TableName: 'playlists',
-  IndexName: 'PlaylistUsers',
-  KeyConditionExpression: "userid = :a",
-  ExpressionAttributeValues: {
-    ":a": "some-user-id"
+module.exports.savePlaylist = (event, context, callback) => {
+  const params = {
+    TableName: 'playlists',
+    Item: {
+      id: 'ea92a194-2d60-35c7-9d56-0e1dba20cd45',
+      name: 'My playlist',
+      userid: 'c16ba271-f7e8-4a41-ac2c-c74d1dc37c5f',
+      tracks: [
+        {
+          artist: 'Radiohead',
+          track: 'airbag' 
+        },
+        {
+          artist: 'Real Estate',
+          track: 'Snow Days'
+        }
+      ]
+    }
   }
+
+  docClient.put(params, function(err, data) {
+    if (err) { 
+      callback(err);
+    }
+    else { 
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          data: data,
+          input: event,
+        }),
+      };
+      
+      callback(null, response);
+    }
+  });
 }
 
-docClient.query(params, function(err, data) {
-  if (err) { 
-    console.log(err);
-  } else { 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        data: data
-      }),
-    };
-    callback(null, response);
+module.exports.getPlaylistsByUserId = (event, context, callback) => {
+  const params = {
+    TableName: 'playlists',
+    IndexName: 'PlaylistUsers',
+    KeyConditionExpression: "userid = :a",
+    ExpressionAttributeValues: {
+      ":a": "c16ba271-f7e8-4a41-ac2c-c74d1dc37c5f"
+    }
   }
-});
-//  docClient.put(params, function(err, data) {
-//    if (err) { 
-//      callback(err);
-//    }
-//    else { 
-//      const response = {
-//        statusCode: 200,
-//        body: JSON.stringify({
-//          data: data,
-//          input: event,
-//        }),
-//      };
-//      
-//      callback(null, response);
-//    }
-//  });
 
+  docClient.query(params, function(err, data) {
+    if (err) { 
+      console.log(err);
+    } else { 
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          data: data
+        }),
+      };
+      callback(null, response);
+    }
+  });
 };
 
