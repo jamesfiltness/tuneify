09e547dcb416e0ce77eed6042e23672253a0fc1d
James Filtness <jamesfiltness@gmail.com>
2017-02-10 08:25:26 +0000

    Remove cookie Middleware as it is not used

--- server/config/express.js
+++ server/config/express.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import application from '../routes/application';
 
 export default function () {
   const app = express();
-  app.use(cookieParser());
 
   // Use https://www.npmjs.com/package/babel-plugin-transform-require-ignore to ignore css imports (CSS modules)
   application(app);
