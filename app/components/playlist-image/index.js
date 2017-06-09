ca5068e85e36484b9301e4514ae50d8a7042713b
James Filtness <jamesfiltness@gmail.com>
2017-06-09 17:31:40 +0100

    Fix Playlist image bug
    Previously if there were more than 4 images available for a playlist they were rendered anyway and hidden from view. This commit fixes that

--- app/components/playlist-image/index.js
+++ app/components/playlist-image/index.js
@@ -11,11 +11,11 @@ export default class PlaylistImage extends React.Component {
   renderImages(imageArr) {
     return imageArr.map((image, i) => {
       return (
-        <img 
+        <img
           key={i}
-          src={image} 
-          alt="Playlist Image" 
-          width="174" 
+          src={image}
+          alt="Playlist Image"
+          width="174"
           height="174"
         />
       )
@@ -24,27 +24,27 @@ export default class PlaylistImage extends React.Component {
 
   render() {
     const images = [];
-    
+
     if (this.props.image) {
       images.push(this.props.image);
     } else {
       const tracks = this.props.tracks;
-      
-     
+
+
       tracks.map((track) => {
-        if(!images.includes(track.image)) {
+        if(!images.includes(track.image) && images.length < 4) {
           images.push(track.image);
         }
       });
-    } 
-     
+    }
+
     const imageContainerClasses = classNames(
       'hero__image-combo',
       `hero__image-combo--count-${!(images.length > 4) ? images.length : 4}`
     )
 
     const imageHtml = this.renderImages(images);
-    
+
     return (
       <div className={imageContainerClasses}>
         {imageHtml}
